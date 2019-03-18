ca32292f3fc83fbc47a7a359729c711071bb145b
Zach Banducci <zbducci@gmail.com>
2019-03-18 18:36:55 -0500

    Extract urls returns a sorted list and added unique url extractor

--- lib/parser/parser.ex
+++ lib/parser/parser.ex
@@ -1,14 +1,26 @@
-defmodule Parser do
+defmodule Explore.Parser do
 
   @doc """
-  Will extract a list of all urls within html
+  Will extract a list of all urls within html and return the list in alphebetical order
 
-  iex> Explore.Parser('<body><a href="google.com"><div><a href="facebook.com"</div></body>')
-  ["google.com", "facebook.com"] 
+  iex> Explore.Parser.extract_urls('<body><a href="google.com"><div><a href="facebook.com"</div></body>')
+  ["facebook.com", "google.com"] 
   """
   def extract_urls(html) do
     html
     |> Floki.find("a")
     |> Floki.attribute("href")
+    |> Enum.sort()
+  end
+
+  @doc """
+  Extracts a list of unique links within html and return the list in alphebetical order
+
+  iex> Explore.Parser.extract_unique_urls("<body><a href=\"google.com\"><a href=\"google.com\"><body>")
+  """
+  def extract_unique_urls(html) do
+    html
+    |> extract_urls()
+    |> Enum.uniq()
   end
 end
--- test/parser/parser_test.exs
+++ test/parser/parser_test.exs
@@ -1,14 +1,20 @@
 defmodule ParserTest do
   use ExUnit.Case, async: true
-  require Parser, as: P
+  require Explore.Parser, as: P
 
   test "html without links results in empty list" do
     html = "<html></html>"
     assert P.extract_urls(html) == []
   end
 
-  test "some method with param" do
+  test "html with links" do
     html = "<html><body><a href=\"google.com\"><div><a href=\"facebook.com\"></div></body></html>"
-    assert P.extract_urls(html) == ["google.com", "facebook.com"]
+    assert P.extract_urls(html) == ["facebook.com", "google.com"]
+  end
+
+  test "html with duplicate links" do
+    html = "<html><body><a href=\"facebook.com\"><div><a href=\"facebook.com\"></div></body></html>"
+    
+    assert P.extract_unique_urls(html) == ["facebook.com"]
   end
 end
