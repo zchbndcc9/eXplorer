63b9d8c48cfea715de427757df99b9645ef48e96
Zach Banducci <zbducci@gmail.com>
2019-03-21 00:03:01 -0500

    Able to retrieve document type and generate an id hash

--- lib/explore/fetcher.ex
+++ lib/explore/fetcher.ex
@@ -13,35 +13,47 @@ defmodule Explore.Fetcher do
     %Document{ url: link }
     |> retrieve_robot()
     |> retrieve_page()
-    |> generate_id()
     |> determine_type()
+    |> generate_id()
     |> check_duplicate()
     |> store_id()
   end
 
   def retrieve_robot(doc = %Document{ url: url }) do
-    status = Grommet.crawlable?("Explorer", url)
+    status = Gollum.crawlable?("Explorer", url)
 
     %Document{ doc | status: status }
   end
 
   def retrieve_page(doc = %Document{ status: :crawlable, url: url }) do
-    { status, body } =
+    { status, body, type } =
       case HTTPoison.get(url) do
-        {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> {:ok, body}
-        {:ok, %HTTPoison.Response{status_code: 404}} -> {:no_page, "Page does not exist"}
-        {_, _} -> {:error, "Some error occured"}
+        {:ok, %HTTPoison.Response{status_code: 200, body: body, headers: type}} -> {:ok, body, type}
+        {:ok, %HTTPoison.Response{status_code: 404}} -> {:no_page, "Page does not exist", "none"}
+        {_, _} -> {:error, "Some error occured", "none"}
       end
 
-    %Document{ doc | status: status, content: body }
+    %Document{ doc | status: status, content: body, type: type }
   end
 
   def retrieve_page(doc) do
     doc
   end
 
+  def determine_type(doc = %Document{ type: "none" }) do
+    doc
+  end
+
+  def determine_type(doc = %Document{ type: type }) do
+    new_type =
+      type
+      |> Enum.find(fn {key, value} -> key == "Content-Type" end)
+      |> elem(1)
+
+    %Document{ doc | type: new_type }
+  end
   def generate_id(doc = %Document{ status: :ok, content: content }) do
-    hash = :crypto.hash(:sha512, content)
+    hash = Base.encode16(:crypto.hash(:sha512, content))
 
     %Document{ doc | id: hash }
   end
@@ -50,10 +62,6 @@ defmodule Explore.Fetcher do
     %Document{ doc | id: :none }
   end
 
-  def determine_type(doc = %Document{ status: :ok, url: url }) do
-    doc
-  end
-  
   def check_duplicate(doc) do
     doc
   end
