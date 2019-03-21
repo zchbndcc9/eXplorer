5a1630432bb060470bbe604862f709ea73b952c5
Zach Banducci <zbducci@gmail.com>
2019-03-20 19:25:18 -0500

    Added frontier queue implementation

---
+++ lib/explore/frontier/queue.ex
@@ -0,0 +1,17 @@
+defmodule Explore.Frontier.Queue do
+  def start_link(_) do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def push(link) do
+    Agent.update(__MODULE__, fn queue -> queue ++ [link] end)
+  end
+
+  def pop() do
+    Agent.get_and_update(__MODULE__, fn [link | queue]-> {link, queue})
+  end
+
+  def empty?() do
+    Agent.get(__MODULE__, fn queue -> Enum.empty?(queue) end)
+  end
+end
